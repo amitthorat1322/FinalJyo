2fe1625c8f520eca53abfb823055c48395f4ff01
amitthorat1322 <amitthorat1322@gmail.com>
2025-04-20 16:03:56 +0530

    FC

--- src/main/java/base/TBase.java
+++ src/main/java/base/TBase.java
@@ -3,14 +3,13 @@ package base;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.BeforeSuite;
+
 
 public class TBase {
 
 	protected static WebDriver driver;
 	
-	@BeforeSuite
+
 	public void initalization()
 	{
 		System.setProperty("webdriver.chrome.driver","E:\\chromedriver.exe");
@@ -22,7 +21,7 @@ public class TBase {
 	}
 	
 	
-	@AfterSuite
+
 	public void tearDown()
 	{
 		if(driver != null)
